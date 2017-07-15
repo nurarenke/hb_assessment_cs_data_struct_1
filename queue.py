7070e5a579c0f6ca21fdd2664e352dcbaed6f7e3
Nura Renke <nurarenke@gmail.com>
2017-07-15 23:13:42 +0000

    finished queue

--- queue.py
+++ queue.py
@@ -55,7 +55,7 @@ class Queue(object):
 
         # FIXME
 
-        pass
+        return self._list.append(item)
 
     def peek(self):
         """Return but don't remove the first item in the queue.
@@ -74,7 +74,7 @@ class Queue(object):
 
         # FIXME
 
-        pass
+        return self._list[0]
 
 if __name__ == "__main__":
     import doctest
